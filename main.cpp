4e834617f7b23276bdf88de6442e8820b29a9a60
unknown <huydanggia630@gmail.com>
2026-04-20 17:19:05 +0700

    Gia lap n luot choi, thong ke so lan thang tung con sau n luot, chua co class Player va Nha Cai, logic cuoc.

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <random>
+using namespace std;
+
+class Dice{
+private:
+    static inline mt19937 rng{random_device{}()};
+    int dice1, dice2;
+    uniform_int_distribution<int> dist;
+    void roll(){
+        dice1=dist(rng);
+        dice2=dist(rng);
+    }
+    int getDice1(){ return dice1; }
+    int getDice2(){ return dice2; }
+public:
+    Dice():dist(1, 6){
+        roll();
+    }
+    int getDiceSum(){
+        roll();
+        return getDice1()+getDice2();
+    }
+};
+
+class Horse{
+private:
+    int pos, goal, no;
+public:
+    Horse(int _pos, int _goal, int _no):pos(_pos), goal(_goal), no(_no){}
+    void move(){ pos++; }
+    bool win(){ return pos>=goal; }
+    int getNo(){ return no; }
+    void reset(){ pos=0; }
+};
+
+class GamePlay{
+private:
+    Dice dice;
+    vector<Horse> horses;
+public:
+    GamePlay(){
+        for(int i=2; i<13; i++){
+            horses.push_back(Horse(0, 13-2*abs(7-i), i));
+        }
+    }
+    void resetAll(){
+        for(auto &a:horses)
+            a.reset();
+    }
+    int run(){
+        resetAll();
+        while(1){
+            int numToMove=dice.getDiceSum();
+            horses[numToMove-2].move();
+            if(horses[numToMove-2].win()){
+                return horses[numToMove-2].getNo();
+            }
+        }
+    }
+};
+
+class Player{
+
+};
+
+class Statistic{
+public:
+    void Test(){
+        cout<<"Nhap so luot choi: ";
+        int n;  cin>>n;
+        vector<int> winner(13);
+        GamePlay play;
+        for(int i=0; i<n; i++)
+            winner[play.run()]++;
+        for(int i=2; i<13; i++)
+            cout<<endl<<"No."<<i<<": "<<winner[i]<<" lan win.";
+    }
+};
+
+int main(){
+    Statistic test;
+    test.Test();
+    return 0;
+}
